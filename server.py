30427aa617e2fddd40794d6383e57e584df42a53
gorenerburak <gorenerburak@gmail.com>
2025-10-18 19:00:59 -0400

    fix landing page

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 '''server.py file'''
-from flask import Flask, request
+from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
+
+@app.route("/")
+def render_index_page():
+    '''Render the landing page'''
+    return render_template('index.html')
 
 @app.route("/emotionDetector")
 def emotion_detector_route():
@@ -13,7 +18,9 @@ def emotion_detector_route():
     if scores['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
 
-    return scores
+    return return (f"For the given statement, the system response is 'anger': {scores['anger']}, "
+            f"'disgust': {scores['disgust']}, 'fear': {scores['fear']}, 'joy': {scores['joy']} and 'sadness': {scores['sadness']}. "
+            f"The dominant emotion is {scores['dominant_emotion']}.")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
