1e9e3ad1509584651e449cae529b1a60f7033571
gorenerburak <gorenerburak@gmail.com>
2025-10-18 17:33:24 -0400

    Task4 is completed!

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,10 +1,14 @@
-from emotion_detection import emotion_detector
+from EmotionDetection.emotion_detection import emotion_detector
 import unittest
 
 class TestEmotionDetector(unittest.TestCase):
     def test_emotion_detector(self):
         # Test joy as dominant emotion
-        result = emotion_detector('I am so happy I am doing this')
+        result = emotion_detector('I love this new technology.')
         self.assertEqual(result['dominant_emotion'], 'joy')
+
+        # Test joy as dominant emotion
+        result = emotion_detector('I hate working long hours')
+        self.assertEqual(result['dominant_emotion'], 'anger')
 
 unittest.main()
